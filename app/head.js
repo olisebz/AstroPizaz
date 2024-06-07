22d48b103b0175c307837d9672769f458708cd63
Yanis Meichtry <yanis.meichtry@netcetera.com>
2024-06-07 15:31:49 +0200

    changed name

--- app/head.js
+++ app/head.js
@@ -1,6 +1,6 @@
 const Head = () => (
   <>
-    <title>Astro Pizza</title>
+    <title>ZMS Pizza</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/img1.webp" type="image/webp" />
   </>
