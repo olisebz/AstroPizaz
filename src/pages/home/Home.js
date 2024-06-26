d2cddf4c5cbe954468750aa42d8bc8f4e0a6fc2b
Yanis Meichtry <yanis@meichtry.dev>
2024-06-26 13:31:40 +0200

    imported the images and did it into the HTML

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,21 @@
 // MainPage.js
 import React from 'react';
 import './Home.css';
+// Importieren der Bilder
+import margheritaImage from '../../images/pizza_margherita.png';
+import pepperoniImage from '../../images/pizza_pepperoni.png';
+import hawaiianImage from '../../images/pizza_hawaiian.png';
+import veggieImage from '../../images/pizza_veggie.png';
+import meatLoversImage from '../../images/pizza_meatlovers.png';
+import buffaloChickenImage from '../../images/pizza_buffalochicken.png';
 
 const pizzas = [
-  { id: 1, name: 'Margherita', description: 'Tomato sauce, mozzarella, and oregano' },
-  { id: 2, name: 'Pepperoni', description: 'Tomato sauce, mozzarella, and pepperoni' },
-  { id: 3, name: 'Hawaiian', description: 'Tomato sauce, cheese, ham, and pineapple' },
-  { id: 4, name: 'Veggie', description: 'Tomato sauce, mozzarella, and assorted vegetables' },
-  { id: 5, name: 'Meat Lovers', description: 'Tomato sauce, mozzarella, and assorted meats' },
-  { id: 6, name: 'Buffalo Chicken', description: 'Buffalo sauce, chicken, and onions' },
+  { id: 1, name: 'Margherita', description: 'Tomato sauce, mozzarella, and oregano', image: margheritaImage },
+  { id: 2, name: 'Pepperoni', description: 'Tomato sauce, mozzarella, and pepperoni', image: pepperoniImage },
+  { id: 3, name: 'Hawaiian', description: 'Tomato sauce, cheese, ham, and pineapple', image: hawaiianImage },
+  { id: 4, name: 'Veggie', description: 'Tomato sauce, mozzarella, and assorted vegetables', image: veggieImage },
+  { id: 5, name: 'Meat Lovers', description: 'Tomato sauce, mozzarella, and assorted meats', image: meatLoversImage },
+  { id: 6, name: 'Buffalo Chicken', description: 'Buffalo sauce, chicken, and onions', image: buffaloChickenImage },
 ];
 
 function Home() {
@@ -19,6 +26,7 @@ function Home() {
       <div className="pizza-container">
         {pizzas.map((pizza) => (
           <div key={pizza.id} className="pizza-box">
+            <img src={pizza.image} alt={pizza.name} className="pizza-image"/>
             <h2>{pizza.name}</h2>
             <p>{pizza.description}</p>
             <button>Add to Cart</button>
