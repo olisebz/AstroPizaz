be1a5715e06d0fadea665e170c49464371b38be2
Yanis Meichtry <yanis@meichtry.dev>
2024-06-26 15:20:59 +0200

    deleted the price on the paypage this is for another story

--- src/pages/pay/PayPage.js
+++ src/pages/pay/PayPage.js
@@ -4,17 +4,16 @@ import './PayPage.css';
 
 function PayPage() {
   const navigate = useNavigate();
-  const totalPrice = JSON.parse(localStorage.getItem('totalPrice'));
 
   const handlePayment = () => {
-    alert("Payment successful!");
+    alert("Payment successful! Your order will be delivered soon.");
     navigate('/');
   };
 
   return (
     <div className="pay-page">
       <h1>Payment Page</h1>
-      <p>Thank you for your order. Please proceed with the payment of ${totalPrice} to complete your purchase.</p>
+      <p>Thank you for your order. Please proceed with the payment to complete your purchase.</p>
       <button onClick={handlePayment} className="pay-button">Pay Now</button>
     </div>
   );
