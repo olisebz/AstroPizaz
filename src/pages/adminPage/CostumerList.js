10dee17bf3dec02cccbca630db1ab184ce15ccbe
Oliver Zenger <oliver@zenger.org>
2024-06-27 10:52:14 +0200

    refactor: Update key in CustomerList to use customer.id instead of index

--- src/pages/adminPage/CostumerList.js
+++ src/pages/adminPage/CostumerList.js
@@ -4,7 +4,7 @@ function CustomerList({ customers, editCustomer, deleteCustomer }) {
   return (
     <div className="customer-list">
       <h2>Customer List</h2>
-      {customers.map((customer, index) => (
+      {customers.map((customer) => (
         <div key={customer.id} className="customer">
           <div>
             <div>{customer.firstName} {customer.lastName}</div>
@@ -12,8 +12,8 @@ function CustomerList({ customers, editCustomer, deleteCustomer }) {
             <div>{customer.phoneNumber}</div>
           </div>
           <div className="customer-buttons">
-            <button className="edit-button" onClick={() => editCustomer(index)}>Edit</button>
-            <button className="delete-button" onClick={() => deleteCustomer(index)}>Delete</button>
+            <button className="edit-button" onClick={() => editCustomer(customer.id)}>Edit</button>
+            <button className="delete-button" onClick={() => deleteCustomer(customer.id)}>Delete</button>
           </div>
         </div>
       ))}
